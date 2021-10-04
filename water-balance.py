646af740c128db857ce4d6d7e33352ad85542063
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-10-03 22:47:13 -0500

    Initial commit

--- water-balance.py
+++ water-balance.py
@@ -6,9 +6,16 @@ import pandas as pd
 C_1 = 0.90
 F_1 = 15
 H_1 = 0.21
+J_1 = 0.09
+
+
+def Calibrate(colm_e):
+    formula = F_1 *(colm_e - J_1) * 0.5
+
+# ADB first value formula : 0.3= F_1*(E4-J_1)*0.5
 
 waterBalance = pd.read_csv('water balanace .csv', usecols=[0])
 
 print(waterBalance)
 
-#print(waterBalance["Allowable Depletion balance (ADB)"])
+# print(waterBalance["Allowable Depletion balance (ADB)"])
