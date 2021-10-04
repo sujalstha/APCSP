3ebd4744c094b3411fceae432b3911ef0e84c1d4
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-10-03 22:08:40 -0500

    Initial commit

--- water-balance.py
+++ water-balance.py
@@ -1,11 +1,4 @@
 # 10/03/2021
 # water balance/soil database code
 
-import openpyxl
-from pathlib import Path
-
-xlsx_file = Path('Water-Example', 'water balanace example.xlsx')
-data_obj = openpyxl.load_workbook(xlsx_file)
-sheet = data_obj.active
-
-print(data_obj)
+import pandas
