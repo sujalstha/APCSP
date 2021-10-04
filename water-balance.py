735e8ef40d38e6d6e0fa20664a25332097364ff3
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-10-03 21:52:46 -0500

    Initial commit

--- water-balance.py
+++ water-balance.py
@@ -5,4 +5,7 @@ import openpyxl
 from pathlib import Path
 
 xlsx_file = Path('Water-Example', 'water balanace example.xlsx')
-print(xlsx_file)
+data_obj = openpyxl.load_workbook(xlsx_file)
+sheet = data_obj.active
+
+print(sheet)
